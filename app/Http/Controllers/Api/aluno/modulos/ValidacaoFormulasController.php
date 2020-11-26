3cd122cf000cce816704628263cec0f6bac324bf
Danilo Saraiva Vicente <36174769+VicenteDNL@users.noreply.github.com>
2020-11-26 11:19:59 -0300

    Create ValidacaoFormulasController.php

---
+++ app/Http/Controllers/Api/aluno/modulos/ValidacaoFormulasController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\aluno\modulos;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ValidacaoFormulasController extends Controller
+{
+    //
+}
