29f7f356f4e7eee5f6aca367a9718928ab19aa9a
Danilo <danilo.saraiva68@gmail.com>
2023-08-05 17:07:23 -0300

    feat(admin, nivel): atualizar recurso crud nivel

--- app/Http/Controllers/Api/admin/modulos/validacaoFormulas/NivelVFController.php
+++ app/Http/Controllers/Api/admin/modulos/validacaoFormulas/NivelVFController.php
@@ -2,156 +2,147 @@
 
 namespace App\Http\Controllers\Api\admin\modulos\validacaoFormulas;
 
+use App\Http\Controllers\Api\Action;
+use App\Http\Controllers\Api\ResponseController;
+use App\Http\Controllers\Api\Type;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\LogicLive\config\Configuracao;
 use App\Http\Controllers\LogicLive\modulos\validacaoFormulas\NivelVF;
 use App\LogicLive;
 use App\NivelMVFLP;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class NivelVFController extends Controller
 {
     private $niveis;
+    private $config;
+    private $logicLive_nivel;
 
-    public function __construct(NivelMVFLP $niveis )
+    public function __construct(NivelMVFLP $niveis)
     {
         $this->niveis = $niveis;
-        $this->config = new Configuracao;
-        $this->logicLive_nivel= new NivelVF;
+        $this->config = new Configuracao();
+        $this->logicLive_nivel = new NivelVF();
     }
+
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
-        try{
-        $data= $this->niveis->orderBy('created_at', 'desc')->paginate(10);
-        return response()->json(['success' => true, 'msg'=>'', 'data'=>$data]);
-
-        }catch(\Exception $e){
-            return response()->json(['success' => false, 'msg'=>'erro no servidor', 'data'=>''],500);
-
+        try {
+            $data = $this->niveis->orderBy('created_at', 'desc')->paginate(10);
+            return ResponseController::json(Type::success, Action::index, $data);
+        } catch(Exception $e) {
+            return ResponseController::json(Type::error, Action::index);
         }
     }
 
     /**
-     * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  Request    $request
+     * @param  NivelMVFLP $nivelMVFLP
+     * @return Response
      */
     public function store(Request $request, NivelMVFLP $nivelMVFLP)
     {
-        try{
-            if($this->config->ativo()){
+        try {
+            if ($this->config->ativo()) {
                 $baseDados = LogicLive::where('tipo', '=', 'modulo1')->get();
                 $baseDados = $baseDados[0];
-                $criadoLogicLive = $this->logicLive_nivel->criarNivel(['mod_codigo'=>$baseDados->meu_id,'niv_nome'=>$request->nome,'niv_descricao'=>$request->descricao,'niv_ativo'=>$request->ativo,]);
+                $criadoLogicLive = $this->logicLive_nivel->criarNivel(['mod_codigo' => $baseDados->meu_id, 'niv_nome' => $request->nome, 'niv_descricao' => $request->descricao, 'niv_ativo' => $request->ativo]);
 
-                if($criadoLogicLive['success']=false){
-                    return response()->json(['success' => false, 'msg'=>$criadoLogicLive['msg'], 'data'=>''],500);
+                if ($criadoLogicLive['success'] = false) {
+                    return ResponseController::json(Type::error, Action::store, null, $criadoLogicLive['msg']);
                 }
-
-                $nivelMVFLP->meu_id_logic_live = $criadoLogicLive['data']['niv_codigo'];
             }
-            $nivelMVFLP->id_modulo = $baseDados->meu_id;
+            $nivelMVFLP->meu_id_logic_live = isset($criadoLogicLive) ? $criadoLogicLive['data']['niv_codigo'] : 0;
+            $nivelMVFLP->id_modulo = isset($baseDados) ? $baseDados->meu_id : 0;
             $nivelMVFLP->nome = $request->nome;
             $nivelMVFLP->descricao = $request->descricao;
             $nivelMVFLP->ativo = $request->ativo;
             $nivelMVFLP->id_recompensa = $request->id_recompensa;
             $nivelMVFLP->save();
-        return response()->json(['success' => true, 'msg'=>'nivel deletado do banco de dados', 'data'=>'']);
 
-    }catch(\Exception $e){
-        return response()->json(['success' => false, 'msg'=>'erro no servidor', 'data'=>''],500);
-    }
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show( $id)
-    {
-
-        return response()->json(['success' => true, 'msg'=>'', 'data'=>$this->niveis->find($id)]);
+            return  ResponseController::json(Type::success, Action::store);
+        } catch(Exception $e) {
+            return  ResponseController::json(Type::error, Action::store);
+        }
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  int      $id
+     * @return Response
      */
-    public function update(Request $request,$id)
+    public function show(int $id)
     {
-        try{
+        $data = $this->niveis->find($id);
+        return ResponseController::json(Type::success, Action::show, $data);
+    }
 
-
-
+    /**
+     *
+     * @param  Request  $request
+     * @param  int      $id
+     * @return Response
+     */
+    public function update(Request $request, int $id)
+    {
+        try {
             $nivelMVFLP = NivelMVFLP::findOrFail($id);
 
-            if($this->config->ativo()){
-                $criadoLogicLive = $this->logicLive_nivel->atualizarNivel($nivelMVFLP->meu_id_logic_live,['niv_nome'=>$request->nome,'niv_descricao'=>$request->descricao,'niv_ativo'=>$request->ativo,'mod_codigo'=>$nivelMVFLP->id_modulo]);
-                if($criadoLogicLive['success']==false){
+            if ($this->config->ativo()) {
+                $criadoLogicLive = $this->logicLive_nivel->atualizarNivel($nivelMVFLP->meu_id_logic_live, ['niv_nome' => $request->nome, 'niv_descricao' => $request->descricao, 'niv_ativo' => $request->ativo, 'mod_codigo' => $nivelMVFLP->id_modulo]);
 
-                    return response()->json(['success' => false, 'msg'=>$criadoLogicLive['msg'], 'data'=>''],500);
+                if ($criadoLogicLive['success'] == false) {
+                    return ResponseController::json(Type::error, Action::update, null, $criadoLogicLive['msg']);
                 }
-
             }
 
             $nivelMVFLP->update($request->all());
             $nivelMVFLP->save();
-            return response()->json(['success' => true, 'msg'=>'nivel atualizado do banco de dados', 'data'=>''], 200);
-
-        }catch(\Exception $e){
-            return response()->json(['success' => false, 'msg'=>'erro no servidor', 'data'=>''],500);
-        }
-
-    }
-
-
-    public function all(){
-        try{
-            $nivelMVFLP = NivelMVFLP::all();
-            return response()->json(['success' => true, 'msg'=>'', 'data'=>$nivelMVFLP]);
-
-        }catch(\Exception $e){
-            return response()->json(['success' => false, 'msg'=>'erro no servidor', 'data'=>''],500);
+            return ResponseController::json(Type::success, Action::update);
+        } catch(Exception $e) {
+            return ResponseController::json(Type::error, Action::update);
         }
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
-    public function destroy($id)
+    public function all()
     {
-        try{
+        try {
+            $nivelMVFLP = NivelMVFLP::all();
+            return ResponseController::json(Type::success, Action::all, $nivelMVFLP);
+        } catch(Exception $e) {
+            return ResponseController::json(Type::error, Action::all);
+        }
+    }
 
-            if($this->config->ativo()){
+    /**
+     * @param  int      $id
+     * @return Response
+     */
+    public function destroy(int $id)
+    {
+        try {
+            if ($this->config->ativo()) {
                 $nivelMVFLP = NivelMVFLP::findOrFail($id);
                 $criadoLogicLive = $this->logicLive_nivel->deletarNivel($nivelMVFLP->meu_id_logic_live);
-                if($criadoLogicLive['success']==false){
 
-                    return response()->json(['success' => false, 'msg'=>$criadoLogicLive['msg'], 'data'=>''],500);
+                if ($criadoLogicLive['success'] == false) {
+                    return ResponseController::json(Type::error, Action::destroy, null, $criadoLogicLive['msg']);
                 }
             }
 
             $nivelMVFLP = NivelMVFLP::findOrFail($id);
             $nivelMVFLP->delete();
-            return response()->json(['success' => true, 'msg'=>'nivel deletado do banco de dados', 'data'=>''], 200);
-
-        }catch(\Exception $e){
-            return response()->json(['success' => false, 'msg'=>'erro no servidor', 'data'=>''],500);
+            return  ResponseController::json(Type::success, Action::destroy);
+        } catch(Exception $e) {
+            return  ResponseController::json(Type::error, Action::destroy);
         }
-
     }
 }
