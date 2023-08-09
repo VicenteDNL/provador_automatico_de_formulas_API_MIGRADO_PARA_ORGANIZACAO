1bf09e124efc32bcac69b808560b7a5f9f7d4a91
Danilo <danilo.saraiva68@gmail.com>
2023-08-09 11:29:45 -0300

    feat(class,regras): aplica tipagem

---
+++ app/Http/Controllers/ModuloArvoreDeRefutacao/Regras/Regras.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\ModuloArvoreDeRefutacao\Regra;
+
+use App\Http\Controllers\ModuloArvoreDeRefutacao\Common\Models\Formula\Predicado;
+
+class Regras
+{
+    /**
+     * Aplica a regra da Dupla Negação em uma instancia do Predicado
+     * @param  Predicado      $predicado
+     * @return RegrasResponse
+     */
+    public function DuplaNeg(Predicado $predicado): RegrasResponse
+    {
+        $centro = clone $predicado;
+        $centro->removeNegacaoPredicado();
+        $centro->removeNegacaoPredicado();
+        return new RegrasResponse(['esquerda' => null, 'centro' => [$centro], 'direita' => null]);
+    }
+
+    /** Aplica a regra da Conjuncao em uma nova instancia do Predicado
+     * @param  Predicado      $predicado
+     * @return RegrasResponse
+     */
+    public function conjuncao(Predicado $predicado): RegrasResponse
+    {
+        $esquerda = clone $predicado->getEsquerdaPredicado();
+        $direita = clone $predicado->getDireitaPredicado();
+        return new RegrasResponse(['esquerda' => null, 'centro' => [ $esquerda, $direita], 'direita' => null]);
+    }
+
+    /** Aplica a regra da Conjuncao Negada em uma nova instancia do Predicado
+     * @param  Predicado      $predicado
+     * @return RegrasResponse
+     */
+    public function conjuncaoNeg(Predicado $predicado): RegrasResponse
+    {
+        $primeiro = clone $predicado->getEsquerdaPredicado();
+        $segundo = clone $predicado->getDireitaPredicado();
+        $primeiro->addNegacaoPredicado();
+        $segundo->addNegacaoPredicado();
+        return new RegrasResponse(['esquerda' => [$primeiro], 'centro' => null, 'direita' => [$segundo]]);
+    }
+
+    /** Aplica a regra da Disjuncao em uma nova instancia do Predicado
+     * @param  Predicado      $predicado
+     * @return RegrasResponse
+     */
+    public function disjuncao(Predicado $predicado): RegrasResponse
+    {
+        $primeiro = clone $predicado->getEsquerdaPredicado();
+        $segundo = clone $predicado->getDireitaPredicado();
+        return new RegrasResponse(['esquerda' => [$primeiro], 'centro' => null, 'direita' => [$segundo]]);
+    }
+
+    /** Aplica a regra da Disjuncao Negada em uma nova instancia do Predicado
+     * @param  Predicado      $predicado
+     * @return RegrasResponse
+     */
+    public function disjuncaoNeg(Predicado $predicado): RegrasResponse
+    {
+        $primeiro = clone $predicado->getEsquerdaPredicado();
+        $segundo = clone $predicado->getDireitaPredicado();
+        $primeiro->addNegacaoPredicado();
+        $segundo->addNegacaoPredicado();
+        return new RegrasResponse(['esquerda' => null, 'centro' => [$primeiro, $segundo], 'direita' => null]);
+    }
+
+    /** Aplica a regra da Condicional em uma nova instancia do Predicado
+     * @param  Predicado      $predicado
+     * @return RegrasResponse
+     */
+    public function condicional(Predicado $predicado): RegrasResponse
+    {
+        $primeiro = clone $predicado->getEsquerdaPredicado();
+        $segundo = clone $predicado->getDireitaPredicado();
+        $primeiro->addNegacaoPredicado();
+        return new RegrasResponse(['esquerda' => [$primeiro], 'centro' => null, 'direita' => [$segundo]]);
+    }
+
+    /** Aplica a regra da Condicional Negado em uma nova instancia do Predicado
+     * @param  Predicado      $predicado
+     * @return RegrasResponse
+     */
+    public function condicionalNeg(Predicado $predicado): RegrasResponse
+    {
+        $primeiro = clone $predicado->getEsquerdaPredicado();
+        $segundo = clone $predicado->getDireitaPredicado();
+        $segundo->addNegacaoPredicado();
+        return new RegrasResponse(['esquerda' => null, 'centro' => [$primeiro, $segundo], 'direita' => null]);
+    }
+
+    /** Aplica a regra da Bicondicional Negado em uma nova instancia do Predicado
+     * @param  Predicado      $predicado
+     * @return RegrasResponse
+     */
+    public function bicondicional(Predicado $predicado): RegrasResponse
+    {
+        $primeiro1 = clone $predicado->getEsquerdaPredicado();
+        $primeiro2 = clone $primeiro1;
+        $segundo1 = clone $predicado->getDireitaPredicado();
+        $segundo2 = clone $segundo1;
+        $primeiro2->addNegacaoPredicado();
+        $segundo2->addNegacaoPredicado();
+        return new RegrasResponse(['esquerda' => [$primeiro1, $segundo1], 'centro' => null, 'direita' => [$primeiro2, $segundo2]]);
+    }
+
+    /** Aplica a regra da Condicional Negado em uma nova instancia do Predicado
+     * @param  Predicado      $predicado
+     * @return RegrasResponse
+     */
+    public function bicondicionalNeg(Predicado $predicado): RegrasResponse
+    {
+        $primeiro1 = clone $predicado->getEsquerdaPredicado();
+        $primeiro2 = clone $primeiro1;
+        $segundo1 = clone $predicado->getDireitaPredicado();
+        $segundo2 = clone $segundo1;
+        $primeiro2->addNegacaoPredicado();
+        $segundo1->addNegacaoPredicado();
+        return new RegrasResponse(['esquerda' => [$primeiro1, $segundo1], 'centro' => null, 'direita' => [$primeiro2, $segundo2]]);
+    }
+}
---
+++ app/Http/Controllers/ModuloArvoreDeRefutacao/Regras/RegrasResponse.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\ModuloArvoreDeRefutacao\Regra;
+
+use App\Http\Controllers\ModuloArvoreDeRefutacao\Common\Models\Formula\Predicado;
+use App\Http\Controllers\ModuloArvoreDeRefutacao\Common\Serializa;
+
+class RegrasResponse extends Serializa
+{
+    /** @var Predicado[] */
+    private ?array $esquerda;
+
+    /** @var Predicado[] */
+    private ?array $centro;
+
+    /** @var Predicado[] */
+    private ?array $direita;
+
+    /**
+     * @return Predicado[]|null
+     */
+    public function getEsquerda(): ?array
+    {
+        return $this->esquerda;
+    }
+
+    /**
+     * @param Predicado[]
+     * @param  array $esquerda
+     * @return void
+     */
+    public function setEsquerda(array $esquerda): void
+    {
+        $this->esquerda = $esquerda;
+    }
+
+    /**
+     * @return Predicado[]|null
+     */
+    public function getCentro(): ?array
+    {
+        return $this->centro;
+    }
+
+    /**
+     * @param Predicado[]
+     * @param  mixed $centro
+     * @return void
+     */
+    public function setCentro($centro): void
+    {
+        $this->centro = $centro;
+    }
+
+    /**
+     * @return Predicado[]|null
+     */
+    public function getDireita(): ?array
+    {
+        return $this->direita;
+    }
+
+    /**
+     * @param Predicado[]
+     * @param  mixed $direita
+     * @return void
+     */
+    public function setDireita($direita): void
+    {
+        $this->direita = $direita;
+    }
+}
