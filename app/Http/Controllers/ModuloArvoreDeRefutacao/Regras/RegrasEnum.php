b3dc9c38bb1191cfab79889a08ced206c3a08d11
Danilo <danilo.saraiva68@gmail.com>
2023-08-09 17:34:33 -0300

    feat(regra,enum): criar enum para regras

---
+++ app/Http/Controllers/ModuloArvoreDeRefutacao/Regras/RegrasEnum.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\ModuloArvoreDeRefutacao\Regras;
+
+enum RegrasEnum
+{
+    case DUPLANEGACAO;
+    case CONJUNCAO;
+    case DISJUNCAONEGADA;
+    case CONDICIONALNEGADA;
+    case DISJUNCAO;
+    case CONDICIONAL;
+    case BICONDICIONAL;
+    case CONJUNCAONEGADA;
+    case BICONDICIONALNEGADA;
+
+    public function descricao(): string
+    {
+        return match ($this) {
+            RegrasEnum::DUPLANEGACAO        => 'Negação Negada',
+            RegrasEnum::CONJUNCAO           => 'Conjunção',
+            RegrasEnum::DISJUNCAONEGADA     => 'Disjunção Negada',
+            RegrasEnum::CONDICIONALNEGADA   => 'Condicional Negado',
+            RegrasEnum::DISJUNCAO           => 'Disjunção',
+            RegrasEnum::CONDICIONAL         => 'Condicional',
+            RegrasEnum::BICONDICIONAL       => 'Bicondicional',
+            RegrasEnum::CONJUNCAONEGADA     => 'Conjunção Negada',
+            RegrasEnum::BICONDICIONALNEGADA => 'Bicondicional Negado',
+        };
+    }
+}
