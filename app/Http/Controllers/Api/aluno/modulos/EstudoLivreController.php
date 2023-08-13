9d521a0b2fab476d55d994a34313207fc2f7369b
Danilo <danilo.saraiva68@gmail.com>
2023-08-13 01:55:49 -0300

    feat(estudo livre): refatorado

--- app/Http/Controllers/Api/aluno/modulos/EstudoLivreController.php
+++ app/Http/Controllers/Api/aluno/modulos/EstudoLivreController.php
@@ -1,140 +1,114 @@
 <?php
 
-namespace App\Http\Controllers\Api\aluno\modulos;
+namespace App\Http\Controllers\Api\Aluno\Modulos;
 
+use App\Http\Controllers\Api\Action;
+use App\Http\Controllers\Api\ResponseController;
+use App\Http\Controllers\Api\Type;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\ModuloArvoreDeRefutacao\Base;
+use App\Http\Controllers\ModuloArvoreDeRefutacao\Common\Exceptions\ArvoreDeRefutacaoExecption;
+use App\Http\Controllers\ModuloArvoreDeRefutacao\Common\Models\Geradores\PassoDerivacao;
+use App\Http\Controllers\ModuloArvoreDeRefutacao\Common\Models\Geradores\PassoFechamento;
+use App\Http\Controllers\ModuloArvoreDeRefutacao\Common\Models\Geradores\PassoInicializacao;
+use App\Http\Controllers\ModuloArvoreDeRefutacao\Common\Models\Geradores\PassoTicagem;
+use App\Http\Requests\API\Aluno\Modulos\EstudoLivre\EstudoLivreAdicionaRequest;
+use App\Http\Requests\API\Aluno\Modulos\EstudoLivre\EstudoLivreArvoreRequest;
+use App\Http\Requests\API\Aluno\Modulos\EstudoLivre\EstudoLivreIniciarRequest;
+use Exception;
 use Illuminate\Http\Request;
 
 class EstudoLivreController extends Controller
 {
-    
-    public function arvore(Request $request){
-
-        $arvore = new Base($request->xml);
-        
-        if(!$arvore->otimizada()){
-            return  response()->json([
-                'success' => false, 
-                'msg'=>$arvore->getError() 
-                ]); 
-        }
-
-        return  response()->json([
-            'success' => true, 
-            'msg'=>'', 
-            'data'=>$arvore->retornoOtimizada()
-            ]);
-
-    }
-
-
-    public function iniciar(Request $request){
-        $arvore = new Base($request->xml);
-
-        if(!$arvore->montarArvore()){
-            return  response()->json(['success' => false, 'msg'=>'Error ar criar arvore', 'data'=>''],500);
-        }
-
-        return  response()->json([
-            'success' => true, 
-            'msg'=>'', 
-            'data'=>[
-                'arvore'=>$arvore->retorno(null,null, null)
-                ]
-            ]);
-
-    }
-
-
-    public function adiciona(Request $request){
-        try{
+    public function arvore(EstudoLivreArvoreRequest $request)
+    {
+        try {
             $arvore = new Base($request->xml);
-            $arvore->setListaPassos($request->inicio['lista']);
-            if(!$arvore->montarArvore($request->inicio['no']['id'],$request->inicio['negacao'])){
-                return  response()->json([
-                    'success' => false, 
-                    'msg'=>$arvore->getError()
-                    ]); 
-            } 
-    
-            return  response()->json([
-                'success' => true, 
-                'msg'=>'', 
-                'data'=>$arvore->retorno(null,null, null)
-                ]);
-        }catch(\Exception $e){
-            return response()->json(['success' => false, 'msg'=>'erro interno', 'data'=>''],500);
+
+            if (!$arvore->arvoreOtimizada()) {
+                return ResponseController::json(Type::error, Action::store, null, $arvore->getErro());
+            }
+
+            return ResponseController::json(Type::success, Action::store, $arvore->imprimirArvore());
+        } catch(Exception $e) {
+            return ResponseController::json(Type::error, Action::store);
         }
     }
 
-
-
-    public function deriva(Request $request){
-
-        $arvore = new Base($request->xml);
-        // Seta todas as configuracoes da arvore
-        $arvore->setAll($request->all(),false,false);
-        if(!$arvore->derivar($request->derivacao['no']['idNo'],$request->derivacao['folhas'],$request->derivacao['regra'])){
-            return  response()->json([
-                'success' => false, 
-                'msg'=>$arvore->getError(), 
-                ]); 
+    public function iniciar(EstudoLivreIniciarRequest $request)
+    {
+        try {
+            $arvore = new Base($request->xml);
+            return ResponseController::json(Type::success, Action::index, $arvore->imprimir());
+        } catch(ArvoreDeRefutacaoExecption $e) {
+            return ResponseController::json(Type::error, Action::index, null, $e->getMessage());
+        } catch(Exception $e) {
+            return ResponseController::json(Type::error, Action::index);
         }
- 
-        return  response()->json([
-            'success' => true, 
-            'msg'=>'',
-             'data'=>$arvore->retorno(null,null, null)
-            ]);
     }
 
+    public function adiciona(EstudoLivreAdicionaRequest $request)
+    {
+        try {
+            $arvore = new Base($request->arvore['formula']['xml']);
+            $arvore->carregarCamposEssenciais($request->all());
+            $passo = new PassoInicializacao($request->passo);
 
-    public function tica(Request $request){
-
-        $arvore = new Base($request->xml);
-        // Seta todas ar configuracoes da arvore
-        $arvore->setAll($request->all(),false,false);
-        if(!$arvore->montarArvore()){
-            return  response()->json(['success' => false, 'msg'=>$arvore->getError()]); 
+            if (!$arvore->tentativaInicializacao($passo)) {
+                return ResponseController::json(Type::error, Action::store, null, $arvore->getErro());
+            }
+            return ResponseController::json(Type::success, Action::store, $arvore->imprimir());
+        } catch(Exception $e) {
+            return ResponseController::json(Type::error, Action::store);
         }
-        
-        if(!$arvore->ticarNo($request->ticar['no'])){
-            return  response()->json([
-                'success' => false, 
-                'msg'=>$arvore->getError(), 
-               ]); 
-        }
-        
-        return  response()->json([
-            'success' => true, 
-            'msg'=>'', 
-            'data'=>$arvore->retorno(null,null,null)
-            ]);
     }
 
+    public function deriva(Request $request)
+    {
+        try {
+            $arvore = new Base($request->arvore['formula']['xml']);
+            $arvore->carregarCamposEssenciais($request->all());
+            $passo = new PassoDerivacao($request->passo);
 
-    public function fecha(Request $request){
-        $arvore = new Base($request->xml);
-        // Seta todas ar configuracoes da arvore
-        $arvore->setAll($request->all(),false, false);
-
-        if(!$arvore->montarArvore()){
-            return  response()->json(['success' => false, 'msg'=>$arvore->getError()]); 
+            if (!$arvore->tentativaDerivacao($passo)) {
+                return ResponseController::json(Type::error, Action::store, null, $arvore->getErro());
+            }
+            return ResponseController::json(Type::success, Action::store, $arvore->imprimir());
+        } catch(Exception $e) {
+            return ResponseController::json(Type::error, Action::store);
         }
+    }
 
-        if(!$arvore->fecharNo($request->fechar['folha'], $request->fechar['no'])){
-            return  response()->json([
-                'success' => false, 
-                'msg'=>$arvore->getError(), 
-                ]);  
+    public function tica(Request $request)
+    {
+        try {
+            $arvore = new Base($request->xml);
+            $arvore->carregarCamposEssenciais($request->all());
+
+            $passo = new PassoTicagem($request->passo);
+
+            if (!$arvore->tentativaTicagem($passo)) {
+                return ResponseController::json(Type::error, Action::update, null, $arvore->getErro());
+            }
+            return ResponseController::json(Type::success, Action::update, $arvore->imprimir());
+        } catch(Exception $e) {
+            return ResponseController::json(Type::error, Action::update);
         }
+    }
 
-        return  response()->json([
-            'success' => true, 
-            'msg'=>'', 
-            'data'=>$arvore->retorno(null,null,null)
-            ]);
-            
+    public function fecha(Request $request)
+    {
+        try {
+            $arvore = new Base($request->xml);
+            $arvore->carregarCamposEssenciais($request->all());
+            $passo = new PassoFechamento($request->passo);
+
+            if (!$arvore->tentativaFechamento($passo)) {
+                return ResponseController::json(Type::error, Action::update, null, $arvore->getErro());
+            }
+            return ResponseController::json(Type::success, Action::update, $arvore->imprimir());
+        } catch(Exception $e) {
+            return ResponseController::json(Type::error, Action::update);
+        }
     }
 }
